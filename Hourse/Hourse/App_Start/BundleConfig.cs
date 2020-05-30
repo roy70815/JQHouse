f219df7980d6cb881f444836ccb2bfc4df98414b
roy70815 <roy70815@gmail.com>
2020-05-30 14:06:00 +0800

    加入Vue

--- Hourse/Hourse/App_Start/BundleConfig.cs
+++ Hourse/Hourse/App_Start/BundleConfig.cs
@@ -20,7 +20,10 @@ namespace Hourse
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/vue.min.js",
+                      "~/Scripts/jquery-3.3.1.min.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
